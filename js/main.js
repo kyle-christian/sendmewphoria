95c11e8556dd60d7117f60b530bb0081ad5de33c
Kyle Christian <kylebolanos@gmail.com>
2022-08-26 12:22:32 -0700

    added styling and functionality

--- js/main.js
+++ js/main.js
@@ -1,9 +1,16 @@
-fetch("https://api.thecatapi.com/v1/images/search")
-    .then(res => res.json()) // parse response as JSON
-    .then(data => {
-      console.log(data)
-      document.querySelector('img').src = data[0].url
-    })
-    .catch(err => {
-        console.log(`error ${err}`)
+document.querySelector('#submit').addEventListener('click', getCat);
+
+function getCat() {
+    document.querySelector('div').style.display = 'contents';
+
+    fetch("https://api.thecatapi.com/v1/images/search")
+        .then(res => res.json()) // parse response as JSON
+        .then(data => {
+        console.log(data)
+        document.querySelector('img').src = data[0].url
+        })
+        .catch(err => {
+            console.log(`error ${err}`)
     });
+};
+
